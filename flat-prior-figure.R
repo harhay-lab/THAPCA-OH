3ce4794a28524d0328044401b7acb5e243878822
Bryan Blette <bletteb18@gmail.com>
2022-02-16 12:42:38 -0500

    Make 4-panel figure on RR scale

--- flat-prior-figure.R
+++ flat-prior-figure.R
@@ -57,10 +57,11 @@ summary(sanon(DeltaVabs ~ grp(TreatRand) + strt(AgeGroup),
 pvf1 <- conf_dist(estimate = coef(mod_primary_rr)[2],
                   stderr = summary(mod_primary_rr)$coefficients[2, 2],
                   type = "logreg", plot_type = "p_val",
-                  n_values = 1e4L, conf_level = c(0.95, 0.90, 0.80),
+                  n_values = 1e4L,
                   null_values = c(0), trans = "exp", alternative = "two_sided",
                   log_yaxis = TRUE, cut_logyaxis = 0.05,
-                  xlab = "Relative Risk",
+                  xlab = "Relative Risk", ylab = "P-value (two-sided)",
+                  ylab_sec = "P-value (one-sided)",
                   together = FALSE, plot_p_limit = 1 - 0.999,
                   plot_counternull = FALSE, plot = TRUE)
 
@@ -68,11 +69,12 @@ pvf1 <- conf_dist(estimate = coef(mod_primary_rr)[2],
 pvf2 <- conf_dist(estimate = coef(mod_primary_rd)[2],
                   stderr = summary(mod_primary_rd)$coefficients[2, 2],
                   type = "logreg", plot_type = "p_val",
-                  n_values = 1e4L, conf_level = c(0.95, 0.90, 0.80),
+                  n_values = 1e4L,
                   null_values = c(0), trans = "identity",
                   alternative = "two_sided",
                   log_yaxis = TRUE, cut_logyaxis = 0.05,
-                  xlab = "Risk Difference",
+                  xlab = "Risk Difference", ylab = "P-value (two-sided)",
+                  ylab_sec = "P-value (one-sided)",
                   together = FALSE, plot_p_limit = 1 - 0.999,
                   plot_counternull = FALSE, plot = TRUE)
 
@@ -80,10 +82,11 @@ pvf2 <- conf_dist(estimate = coef(mod_primary_rd)[2],
 pvf3 <- conf_dist(estimate = coef(mod_secondary_rr)[2],
                   stderr = summary(mod_secondary_rr)$coefficients[2, 2],
                   type = "logreg", plot_type = "p_val",
-                  n_values = 1e4L, conf_level = c(0.95, 0.90, 0.80),
+                  n_values = 1e4L,
                   null_values = c(0), trans = "exp", alternative = "two_sided",
                   log_yaxis = TRUE, cut_logyaxis = 0.05,
-                  xlab = "Relative Risk",
+                  xlab = "Relative Risk", ylab = "P-value (two-sided)",
+                  ylab_sec = "P-value (one-sided)",
                   together = FALSE, plot_p_limit = 1 - 0.999,
                   plot_counternull = FALSE, plot = TRUE)
 
@@ -91,11 +94,12 @@ pvf3 <- conf_dist(estimate = coef(mod_secondary_rr)[2],
 pvf4 <- conf_dist(estimate = coef(mod_secondary_rd)[2],
                   stderr = summary(mod_secondary_rd)$coefficients[2, 2],
                   type = "logreg", plot_type = "p_val",
-                  n_values = 1e4L, conf_level = c(0.95, 0.90, 0.80),
+                  n_values = 1e4L,
                   null_values = c(0), trans = "identity",
                   alternative = "two_sided",
                   log_yaxis = TRUE, cut_logyaxis = 0.05,
-                  xlab = "Risk Difference",
+                  xlab = "Risk Difference", ylab = "P-value (two-sided)",
+                  ylab_sec = "P-value (one-sided)",
                   together = FALSE, plot_p_limit = 1 - 0.999,
                   plot_counternull = FALSE, plot = TRUE)
 
@@ -136,11 +140,11 @@ plot_data$barriers[plot_data$x < 1/1.25] <- 2
 fig1 <- ggplot(plot_data, aes(x = x, y = y, group = barriers)) +
   geom_line() +
   labs(x = "Relative Risk", y = "Density") +
-  scale_x_continuous(expand = c(0, 0),
+  scale_x_continuous(expand = c(0, 0), trans = "log",
                      labels = seq(0.5, 4, by = 0.5),
                      breaks = seq(0.5, 4, by = 0.5)) +
   scale_y_continuous(expand = c(0, 0)) +
-  coord_cartesian(xlim = c(0.5, 4.5),
+  coord_cartesian(xlim = c(0.5, 6.2),
                   ylim = c(0, 1)) +
   geom_ribbon(aes(ymin=0, ymax=y, fill=factor(barriers)),
               show.legend = FALSE) +
@@ -156,27 +160,27 @@ fig1 <- ggplot(plot_data, aes(x = x, y = y, group = barriers)) +
   geom_segment(inherit.aes = FALSE,
                data =
                  subset(plot_data, x > 1/1.05 & x < 1.05)[
-                   c(1, 3, 8, 10), ],
+                   c(1, 3, 6, 8), ],
                aes(x = x, y = 0, xend = x, yend = y)) +
-  annotate("text", x = 3.2, y = 0.9, size = 3, hjust = 0,
+  annotate("text", x = 2.7, y = 0.8, size = 3, hjust = 0,
            label = paste0("P(Benefit) = ", 1 - round(any_harm, 2),
                           "\n", "P(Harm) = ", round(any_harm, 2),
                           "\n", "P(Severe Harm) = ",
                           round(severe_harm, 2),
                           "\n", "ROPE = ", round(rope, 2))) +
-  annotate("rect", xmin = 3.05, xmax = 3.15, ymin = 0.943, ymax = 0.973,
+  annotate("rect", xmin = 2.4, xmax = 2.6, ymin = 0.905, ymax = 0.965,
            fill = "#DEEBF7") +
-  annotate("rect", xmin = 3.05, xmax = 3.15, ymin = 0.903, ymax = 0.933,
+  annotate("rect", xmin = 2.4, xmax = 2.6, ymin = 0.813, ymax = 0.873,
            fill = "#9ECAE1") +
-  annotate("rect", xmin = 2.95, xmax = 3.05, ymin = 0.903, ymax = 0.933,
+  annotate("rect", xmin = 2.2, xmax = 2.4, ymin = 0.813, ymax = 0.873,
            fill = "#3182BD") +
-  annotate("rect", xmin = 3.05, xmax = 3.15, ymin = 0.863, ymax = 0.893,
+  annotate("rect", xmin = 2.4, xmax = 2.6, ymin = 0.721, ymax = 0.781,
            fill = "#3182BD") +
-  annotate("segment", x = 3.06, xend = 3.06, y = 0.853, yend = 0.823) +
-  annotate("segment", x = 3.08, xend = 3.08, y = 0.853, yend = 0.823) +
-  annotate("segment", x = 3.10, xend = 3.10, y = 0.853, yend = 0.823) +
-  annotate("segment", x = 3.12, xend = 3.12, y = 0.853, yend = 0.823) +
-  annotate("segment", x = 3.14, xend = 3.14, y = 0.853, yend = 0.823) +
+  annotate("segment", x = 2.42, xend = 2.42, y = 0.689, yend = 0.629) +
+  annotate("segment", x = 2.46, xend = 2.46, y = 0.689, yend = 0.629) +
+  annotate("segment", x = 2.50, xend = 2.50, y = 0.689, yend = 0.629) +
+  annotate("segment", x = 2.54, xend = 2.54, y = 0.689, yend = 0.629) +
+  annotate("segment", x = 2.58, xend = 2.58, y = 0.689, yend = 0.629) +
   geom_vline(xintercept = 1, color = "black",
              linetype = 1)
 
@@ -278,11 +282,11 @@ plot_data$barriers[plot_data$x < 1/1.25] <- 2
 fig3 <- ggplot(plot_data, aes(x = x, y = y, group = barriers)) +
   geom_line() +
   labs(x = "Relative Risk", y = "Density") +
-  scale_x_continuous(expand = c(0, 0),
-                     labels = seq(0.5, 2.5, by = 0.5),
-                     breaks = seq(0.5, 2.5, by = 0.5)) +
+  scale_x_continuous(expand = c(0, 0), trans = "log",
+                     labels = seq(0.5, 4, by = 0.5),
+                     breaks = seq(0.5, 4, by = 0.5)) +
   scale_y_continuous(expand = c(0, 0)) +
-  coord_cartesian(xlim = c(0.5, 2.5),
+  coord_cartesian(xlim = c(0.5, 6.2),
                   ylim = c(0, 2)) +
   geom_ribbon(aes(ymin=0, ymax=y, fill=factor(barriers)),
               show.legend = FALSE) +
@@ -298,27 +302,27 @@ fig3 <- ggplot(plot_data, aes(x = x, y = y, group = barriers)) +
   geom_segment(inherit.aes = FALSE,
                data =
                  subset(plot_data, x > 1/1.05 & x < 1.05)[
-                   c(1, 6, 18, 23), ],
+                   c(1, 7, 19, 25), ],
                aes(x = x, y = 0, xend = x, yend = y)) +
-  annotate("text", x = 1.8, y = 1.8, size = 3, hjust = 0,
+  annotate("text", x = 2.7, y = 1.6, size = 3, hjust = 0,
            label = paste0("P(Benefit) = ", 1 - round(any_harm, 2),
                           "\n", "P(Harm) = ", round(any_harm, 2),
                           "\n", "P(Severe Harm) < 0.01",
                           #round(severe_harm, 2),
                           "\n", "ROPE = ", round(rope, 2))) +
-  annotate("rect", xmin = 1.73, xmax = 1.78, ymin = 1.88, ymax = 1.95,
+  annotate("rect", xmin = 2.4, xmax = 2.6, ymin = 1.81, ymax = 1.93,
            fill = "#DEEBF7") +
-  annotate("rect", xmin = 1.73, xmax = 1.78, ymin = 1.8, ymax = 1.87,
+  annotate("rect", xmin = 2.4, xmax = 2.6, ymin = 1.626, ymax = 1.746,
            fill = "#9ECAE1") +
-  annotate("rect", xmin = 1.68, xmax = 1.73, ymin = 1.8, ymax = 1.87,
+  annotate("rect", xmin = 2.2, xmax = 2.4, ymin = 1.626, ymax = 1.746,
            fill = "#3182BD") +
-  annotate("rect", xmin = 1.73, xmax = 1.78, ymin = 1.72, ymax = 1.79,
+  annotate("rect", xmin = 2.4, xmax = 2.6, ymin = 1.442, ymax = 1.562,
            fill = "#3182BD") +
-  annotate("segment", x = 1.735, xend = 1.735, y = 1.64, yend = 1.71) +
-  annotate("segment", x = 1.745, xend = 1.745, y = 1.64, yend = 1.71) +
-  annotate("segment", x = 1.755, xend = 1.755, y = 1.64, yend = 1.71) +
-  annotate("segment", x = 1.765, xend = 1.765, y = 1.64, yend = 1.71) +
-  annotate("segment", x = 1.775, xend = 1.775, y = 1.64, yend = 1.71) +
+  annotate("segment", x = 2.42, xend = 2.42, y = 1.378, yend = 1.258) +
+  annotate("segment", x = 2.46, xend = 2.46, y = 1.378, yend = 1.258) +
+  annotate("segment", x = 2.50, xend = 2.50, y = 1.378, yend = 1.258) +
+  annotate("segment", x = 2.54, xend = 2.54, y = 1.378, yend = 1.258) +
+  annotate("segment", x = 2.58, xend = 2.58, y = 1.378, yend = 1.258) +
   geom_vline(xintercept = 1, color = "black",
              linetype = 1)
 
@@ -416,12 +420,28 @@ dev.off()
 # All RR scale analyses
 figure3 <- multi_panel_figure(columns = 2, rows = 2, width = 240,
                               height = 125)
-figure3 <- fill_panel(figure3, pvf1$plot)
+figure3 <- fill_panel(figure3,
+                      pvf1$plot +
+                        geom_hline(yintercept = c(0.05, 0.1, 0.2),
+                                   linetype = 2) +
+                        theme(axis.text.x = element_text(size = 8),
+                              axis.text.y = element_text(size = 8),
+                              axis.title.y.left = element_text(size = 10),
+                              axis.title.y.right = element_text(size = 10),
+                              axis.title.x = element_text(size = 10)))
 figure3 <- fill_panel(figure3, fig1)
-figure3 <- fill_panel(figure3, pvf3$plot)
+figure3 <- fill_panel(figure3,
+                      pvf3$plot +
+                        geom_hline(yintercept = c(0.05, 0.1, 0.2),
+                                   linetype = 2) +
+                        theme(axis.text.x = element_text(size = 8),
+                              axis.text.y = element_text(size = 8),
+                              axis.title.y.left = element_text(size = 10),
+                              axis.title.y.right = element_text(size = 10),
+                              axis.title.x = element_text(size = 10)))
 figure3 <- fill_panel(figure3, fig3)
 
 # Output pdf of RR figure, dims may need to be changed
-pdf("flat-prior-RR-figure-test.pdf", width = 10, height = 6)
+pdf("pvf-flat-prior-RR-figure.pdf", width = 9.75, height = 6)
 figure3
 dev.off()
