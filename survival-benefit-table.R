3f4bb22d27d8c106c6479563847a58c965e4d06f
Bryan Blette <bletteb18@gmail.com>
2022-02-18 09:49:38 -0500

    Convert risk difference from decimal to percent in benefit tables

--- survival-benefit-table.R
+++ survival-benefit-table.R
@@ -13,7 +13,7 @@ library(sanon)
 library(officer)
 
 # Load trial data
-dat <- read.csv("../outcomes.csv")
+dat <- read.csv("/Users/blette/Downloads/outcomes.csv")
 dat_primary <- dat[dat$Primary == 1 & dat$PrimaryEndpoint != 97, ]
 dat_primary$Trt <- abs(2 - dat_primary$TreatRand)
 dat_primary$AgeFactor <- as.factor(dat_primary$AgeGroup)
@@ -63,16 +63,16 @@ pred0_so <- posterior_epred(so_mod,
                             newdata = mutate(so_mod$data, Trt = 0))
 
 ratio_so <- apply(pred1_so, 1, mean) / apply(pred0_so, 1, mean)
-diff_so <- apply(pred1_so, 1, mean) - apply(pred0_so, 1, mean)
+diff_so <- 100*(apply(pred1_so, 1, mean) - apply(pred0_so, 1, mean))
 
 so_med_d <- median(diff_so)
 so_low_d <- quantile(diff_so, 0.025)
 so_up_d <- quantile(diff_so, 0.975)
 so0.00 <- mean(diff_so > 0)
-so0.02 <- mean(diff_so > 0.02)
-so0.05 <- mean(diff_so > 0.05)
-so0.10 <- mean(diff_so > 0.1)
-so0.15 <- mean(diff_so > 0.15)
+so0.02 <- mean(diff_so > 2)
+so0.05 <- mean(diff_so > 5)
+so0.10 <- mean(diff_so > 10)
+so0.15 <- mean(diff_so > 15)
 
 so_med_r <- median(ratio_so)
 so_low_r <- quantile(ratio_so, 0.025)
@@ -98,16 +98,16 @@ pred0_mo <- posterior_epred(mo_mod,
                             newdata = mutate(mo_mod$data, Trt = 0))
 
 ratio_mo <- apply(pred1_mo, 1, mean) / apply(pred0_mo, 1, mean)
-diff_mo <- apply(pred1_mo, 1, mean) - apply(pred0_mo, 1, mean)
+diff_mo <- 100*(apply(pred1_mo, 1, mean) - apply(pred0_mo, 1, mean))
 
 mo_med_d <- median(diff_mo)
 mo_low_d <- quantile(diff_mo, 0.025)
 mo_up_d <- quantile(diff_mo, 0.975)
 mo0.00 <- mean(diff_mo > 0)
-mo0.02 <- mean(diff_mo > 0.02)
-mo0.05 <- mean(diff_mo > 0.05)
-mo0.10 <- mean(diff_mo > 0.1)
-mo0.15 <- mean(diff_mo > 0.15)
+mo0.02 <- mean(diff_mo > 2)
+mo0.05 <- mean(diff_mo > 5)
+mo0.10 <- mean(diff_mo > 10)
+mo0.15 <- mean(diff_mo > 15)
 
 mo_med_r <- median(ratio_mo)
 mo_low_r <- quantile(ratio_mo, 0.025)
@@ -133,16 +133,16 @@ pred0_wo <- posterior_epred(wo_mod,
                             newdata = mutate(wo_mod$data, Trt = 0))
 
 ratio_wo <- apply(pred1_wo, 1, mean) / apply(pred0_wo, 1, mean)
-diff_wo <- apply(pred1_wo, 1, mean) - apply(pred0_wo, 1, mean)
+diff_wo <- 100*(apply(pred1_wo, 1, mean) - apply(pred0_wo, 1, mean))
 
 wo_med_d <- median(diff_wo)
 wo_low_d <- quantile(diff_wo, 0.025)
 wo_up_d <- quantile(diff_wo, 0.975)
 wo0.00 <- mean(diff_wo > 0)
-wo0.02 <- mean(diff_wo > 0.02)
-wo0.05 <- mean(diff_wo > 0.05)
-wo0.10 <- mean(diff_wo > 0.1)
-wo0.15 <- mean(diff_wo > 0.15)
+wo0.02 <- mean(diff_wo > 2)
+wo0.05 <- mean(diff_wo > 5)
+wo0.10 <- mean(diff_wo > 10)
+wo0.15 <- mean(diff_wo > 15)
 
 wo_med_r <- median(ratio_wo)
 wo_low_r <- quantile(ratio_wo, 0.025)
@@ -169,16 +169,16 @@ pred0_sn <- posterior_epred(sn_mod,
                             newdata = mutate(sn_mod$data, Trt = 0))
 
 ratio_sn <- apply(pred1_sn, 1, mean) / apply(pred0_sn, 1, mean)
-diff_sn <- apply(pred1_sn, 1, mean) - apply(pred0_sn, 1, mean)
+diff_sn <- 100*(apply(pred1_sn, 1, mean) - apply(pred0_sn, 1, mean))
 
 sn_med_d <- median(diff_sn)
 sn_low_d <- quantile(diff_sn, 0.025)
 sn_up_d <- quantile(diff_sn, 0.975)
 sn0.00 <- mean(diff_sn > 0)
-sn0.02 <- mean(diff_sn > 0.02)
-sn0.05 <- mean(diff_sn > 0.05)
-sn0.10 <- mean(diff_sn > 0.1)
-sn0.15 <- mean(diff_sn > 0.15)
+sn0.02 <- mean(diff_sn > 2)
+sn0.05 <- mean(diff_sn > 5)
+sn0.10 <- mean(diff_sn > 10)
+sn0.15 <- mean(diff_sn > 15)
 
 sn_med_r <- median(ratio_sn)
 sn_low_r <- quantile(ratio_sn, 0.025)
@@ -204,16 +204,16 @@ pred0_mn <- posterior_epred(mn_mod,
                             newdata = mutate(mn_mod$data, Trt = 0))
 
 ratio_mn <- apply(pred1_mn, 1, mean) / apply(pred0_mn, 1, mean)
-diff_mn <- apply(pred1_mn, 1, mean) - apply(pred0_mn, 1, mean)
+diff_mn <- 100*(apply(pred1_mn, 1, mean) - apply(pred0_mn, 1, mean))
 
 mn_med_d <- median(diff_mn)
 mn_low_d <- quantile(diff_mn, 0.025)
 mn_up_d <- quantile(diff_mn, 0.975)
 mn0.00 <- mean(diff_mn > 0)
-mn0.02 <- mean(diff_mn > 0.02)
-mn0.05 <- mean(diff_mn > 0.05)
-mn0.10 <- mean(diff_mn > 0.1)
-mn0.15 <- mean(diff_mn > 0.15)
+mn0.02 <- mean(diff_mn > 2)
+mn0.05 <- mean(diff_mn > 5)
+mn0.10 <- mean(diff_mn > 10)
+mn0.15 <- mean(diff_mn > 15)
 
 mn_med_r <- median(ratio_mn)
 mn_low_r <- quantile(ratio_mn, 0.025)
@@ -237,16 +237,16 @@ pred0_wn <- posterior_epred(wn_mod,
                             newdata = mutate(wn_mod$data, Trt = 0))
 
 ratio_wn <- apply(pred1_wn, 1, mean) / apply(pred0_wn, 1, mean)
-diff_wn <- apply(pred1_wn, 1, mean) - apply(pred0_wn, 1, mean)
+diff_wn <- 100*(apply(pred1_wn, 1, mean) - apply(pred0_wn, 1, mean))
 
 wn_med_d <- median(diff_wn)
 wn_low_d <- quantile(diff_wn, 0.025)
 wn_up_d <- quantile(diff_wn, 0.975)
 wn0.00 <- mean(diff_wn > 0)
-wn0.02 <- mean(diff_wn > 0.02)
-wn0.05 <- mean(diff_wn > 0.05)
-wn0.10 <- mean(diff_wn > 0.1)
-wn0.15 <- mean(diff_wn > 0.15)
+wn0.02 <- mean(diff_wn > 2)
+wn0.05 <- mean(diff_wn > 5)
+wn0.10 <- mean(diff_wn > 10)
+wn0.15 <- mean(diff_wn > 15)
 
 wn_med_r <- median(ratio_wn)
 wn_low_r <- quantile(ratio_wn, 0.025)
@@ -273,16 +273,16 @@ pred0_sp <- posterior_epred(sp_mod,
                             newdata = mutate(sp_mod$data, Trt = 0))
 
 ratio_sp <- apply(pred1_sp, 1, mean) / apply(pred0_sp, 1, mean)
-diff_sp <- apply(pred1_sp, 1, mean) - apply(pred0_sp, 1, mean)
+diff_sp <- 100*(apply(pred1_sp, 1, mean) - apply(pred0_sp, 1, mean))
 
 sp_med_d <- median(diff_sp)
 sp_low_d <- quantile(diff_sp, 0.025)
 sp_up_d <- quantile(diff_sp, 0.975)
 sp0.00 <- mean(diff_sp > 0)
-sp0.02 <- mean(diff_sp > 0.02)
-sp0.05 <- mean(diff_sp > 0.05)
-sp0.10 <- mean(diff_sp > 0.1)
-sp0.15 <- mean(diff_sp > 0.15)
+sp0.02 <- mean(diff_sp > 2)
+sp0.05 <- mean(diff_sp > 5)
+sp0.10 <- mean(diff_sp > 10)
+sp0.15 <- mean(diff_sp > 15)
 
 sp_med_r <- median(ratio_sp)
 sp_low_r <- quantile(ratio_sp, 0.025)
@@ -307,16 +307,16 @@ pred0_mp <- posterior_epred(mp_mod,
                             newdata = mutate(mp_mod$data, Trt = 0))
 
 ratio_mp <- apply(pred1_mp, 1, mean) / apply(pred0_mp, 1, mean)
-diff_mp <- apply(pred1_mp, 1, mean) - apply(pred0_mp, 1, mean)
+diff_mp <- 100*(apply(pred1_mp, 1, mean) - apply(pred0_mp, 1, mean))
 
 mp_med_d <- median(diff_mp)
 mp_low_d <- quantile(diff_mp, 0.025)
 mp_up_d <- quantile(diff_mp, 0.975)
 mp0.00 <- mean(diff_mp > 0)
-mp0.02 <- mean(diff_mp > 0.02)
-mp0.05 <- mean(diff_mp > 0.05)
-mp0.10 <- mean(diff_mp > 0.1)
-mp0.15 <- mean(diff_mp > 0.15)
+mp0.02 <- mean(diff_mp > 2)
+mp0.05 <- mean(diff_mp > 5)
+mp0.10 <- mean(diff_mp > 10)
+mp0.15 <- mean(diff_mp > 15)
 
 mp_med_r <- median(ratio_mp)
 mp_low_r <- quantile(ratio_mp, 0.025)
@@ -341,16 +341,16 @@ pred0_wp <- posterior_epred(wp_mod,
                             newdata = mutate(wp_mod$data, Trt = 0))
 
 ratio_wp <- apply(pred1_wp, 1, mean) / apply(pred0_wp, 1, mean)
-diff_wp <- apply(pred1_wp, 1, mean) - apply(pred0_wp, 1, mean)
+diff_wp <- 100*(apply(pred1_wp, 1, mean) - apply(pred0_wp, 1, mean))
 
 wp_med_d <- median(diff_wp)
 wp_low_d <- quantile(diff_wp, 0.025)
 wp_up_d <- quantile(diff_wp, 0.975)
 wp0.00 <- mean(diff_wp > 0)
-wp0.02 <- mean(diff_wp > 0.02)
-wp0.05 <- mean(diff_wp > 0.05)
-wp0.10 <- mean(diff_wp > 0.1)
-wp0.15 <- mean(diff_wp > 0.15)
+wp0.02 <- mean(diff_wp > 2)
+wp0.05 <- mean(diff_wp > 5)
+wp0.10 <- mean(diff_wp > 10)
+wp0.15 <- mean(diff_wp > 15)
 
 wp_med_r <- median(ratio_wp)
 wp_low_r <- quantile(ratio_wp, 0.025)
@@ -370,48 +370,48 @@ df <- data.frame("Prior Belief" = c("Optimistic", "", "",
                                     "Neutral", "", "",
                                     "Pessimistic", "", ""),
                  "Strength" = rep(c("Weak", "Moderate", "Strong"), 3),
-                 "Median" = c(paste(round(wo_med_d, 2), " (",
-                                    round(wo_low_d, 2), ", ",
-                                    round(wo_up_d, 2), ")", sep = ""),
-                              paste(round(mo_med_d, 2), " (",
-                                    round(mo_low_d, 2), ", ",
-                                    round(mo_up_d, 2), ")", sep = ""),
-                              paste(round(so_med_d, 2), " (",
-                                    round(so_low_d, 2), ", ",
-                                    round(so_up_d, 2), ")", sep = ""),
-                              paste(round(wn_med_d, 2), " (",
-                                    round(wn_low_d, 2), ", ",
-                                    round(wn_up_d, 2), ")", sep = ""),
-                              paste(round(mn_med_d, 2), " (",
-                                    round(mn_low_d, 2), ", ",
-                                    round(mn_up_d, 2), ")", sep = ""),
-                              paste(round(sn_med_d, 2), " (",
-                                    round(sn_low_d, 2), ", ",
-                                    round(sn_up_d, 2), ")", sep = ""),
-                              paste(round(wp_med_d, 2), " (",
-                                    round(wp_low_d, 2), ", ",
-                                    round(wp_up_d, 2), ")", sep = ""),
-                              paste(round(mp_med_d, 2), " (",
-                                    round(mp_low_d, 2), ", ",
-                                    round(mp_up_d, 2), ")", sep = ""),
-                              paste(round(sp_med_d, 2), " (",
-                                    round(sp_low_d, 2), ", ",
-                                    round(sp_up_d, 2), ")", sep = "")),
+                 "Median" = c(paste(round(wo_med_d, 1), " (",
+                                    round(wo_low_d, 1), ", ",
+                                    round(wo_up_d, 1), ")", sep = ""),
+                              paste(round(mo_med_d, 1), " (",
+                                    round(mo_low_d, 1), ", ",
+                                    round(mo_up_d, 1), ")", sep = ""),
+                              paste(round(so_med_d, 1), " (",
+                                    round(so_low_d, 1), ", ",
+                                    round(so_up_d, 1), ")", sep = ""),
+                              paste(round(wn_med_d, 1), " (",
+                                    round(wn_low_d, 1), ", ",
+                                    round(wn_up_d, 1), ")", sep = ""),
+                              paste(round(mn_med_d, 1), " (",
+                                    round(mn_low_d, 1), ", ",
+                                    round(mn_up_d, 1), ")", sep = ""),
+                              paste(round(sn_med_d, 1), " (",
+                                    round(sn_low_d, 1), ", ",
+                                    round(sn_up_d, 1), ")", sep = ""),
+                              paste(round(wp_med_d, 1), " (",
+                                    round(wp_low_d, 1), ", ",
+                                    round(wp_up_d, 1), ")", sep = ""),
+                              paste(round(mp_med_d, 1), " (",
+                                    round(mp_low_d, 1), ", ",
+                                    round(mp_up_d, 1), ")", sep = ""),
+                              paste(round(sp_med_d, 1), " (",
+                                    round(sp_low_d, 1), ", ",
+                                    round(sp_up_d, 1), ")", sep = "")),
                  ">0" = 100*round(c(wo0.00, mo0.00, so0.00,
                                     wn0.00, mn0.00, sn0.00,
                                     wp0.00, mp0.00, sp0.00), 2),
-                 ">0.02" = 100*round(c(wo0.02, mo0.02, so0.02,
-                                       wn0.02, mn0.02, sn0.02,
-                                       wp0.02, mp0.02, sp0.02), 2),
-                 ">0.05" = 100*round(c(wo0.05, mo0.05, so0.05,
-                                       wn0.05, mn0.05, sn0.05,
-                                       wp0.05, mp0.05, sp0.05), 2),
-                 ">0.1" = 100*round(c(wo0.10, mo0.10, so0.10,
-                                      wn0.10, mn0.10, sn0.10,
-                                      wp0.10, mp0.10, sp0.10), 2),
-                 ">0.15" = 100*round(c(wo0.15, mo0.15, so0.15,
-                                       wn0.15, mn0.15, sn0.15,
-                                       wp0.15, mp0.15, sp0.15), 2))
+                 ">2" = 100*round(c(wo0.02, mo0.02, so0.02,
+                                    wn0.02, mn0.02, sn0.02,
+                                    wp0.02, mp0.02, sp0.02), 2),
+                 ">5" = 100*round(c(wo0.05, mo0.05, so0.05,
+                                    wn0.05, mn0.05, sn0.05,
+                                    wp0.05, mp0.05, sp0.05), 2),
+                 ">10" = 100*round(c(wo0.10, mo0.10, so0.10,
+                                     wn0.10, mn0.10, sn0.10,
+                                     wp0.10, mp0.10, sp0.10), 2),
+                 ">15" = 100*round(c(wo0.15, mo0.15, so0.15,
+                                     wn0.15, mn0.15, sn0.15,
+                                     wp0.15, mp0.15, sp0.15), 2))
 
 ft <- flextable(df) %>%
   theme_zebra()
@@ -476,7 +476,7 @@ ft2 <- flextable(df2) %>%
   theme_zebra()
 ft2 <- width(ft2, j = 3, width = 1.5)
 
-# Export table to Word (pull flat prior values from flat-prior-figure.R)
+# Export table to Word (man. pull flat prior values from flat-prior-figure.R)
 read_docx() %>% 
   body_add_par("Benefit Table (RR Scale)") %>% 
   body_add_flextable(value = ft2) %>% 
